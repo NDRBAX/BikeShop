f3f6b8f08fa36cfbc98238343b3e9b917921161a
NDRBAX <andre.bacaliu@icloud.com>
2022-04-14 00:53:04 +0200

    Added management of shipping costs

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,31 @@ var dataBike = [
     { name: "LIK099", url: "/images/bike-6.jpg", price: 869 },
 ]
 
+// CALCUL DES FRAIS DE PORT
+
+var calculTotalCmd = (dataCardBike) => {
+    var nbProduits = 0;
+    var totalCmd = 0;
+
+    for (var i = 0; i < dataCardBike.length; i++) {
+        nbProduits += dataCardBike[i].quantity;
+        totalCmd += dataCardBike[i].quantity * dataCardBike[i].price;
+    }
+    // 👉 Pour chaque produit acheté, 30€ de frais de port
+    var montantFraisPort = nbProduits * 30;
+    // 👉 Si la commande dépasse 4000€ (hors frais de port), les frais de port sont gratuits
+    if (totalCmd > 4000) {
+        montantFraisPort = 0;
+        // 👉 Si la commande dépasse 2000€ (hors frais de port), réduction de 50%
+    } else if (totalCmd > 2000) {
+        montantFraisPort = montantFraisPort / 2;
+    }
+
+    totalCmd += montantFraisPort;
+    return { montantFraisPort, totalCmd };
+}
+
+
 /* 
 ? ******* GET home page ******* */
 
@@ -23,14 +48,30 @@ router.get('/', function(req, res, next) {
 /* 
 ? ******* SHOP PAGE ******* */
 
-router.get('/shop', function(req, res, next) {
+router.get('/shop', async function(req, res, next) {
+    if (req.session.dataCardBike == undefined) {
+        req.session.dataCardBike = [];
+    }
+    var total = calculTotalCmd(req.session.dataCardBike);
+
+    // Frais de port
+    var montantFraisPort = total.montantFraisPort;
+
+    // Total commande
+    var montantCommande = total.totalCmd;
+    console.log(req.session, montantFraisPort, montantCommande);
+
+    res.render('shop', { dataCardBike: req.session.dataCardBike, montantFraisPort, montantCommande });
+});
+
+router.get('/add-shop', async function(req, res, next) {
     if (req.session.dataCardBike == undefined) {
         req.session.dataCardBike = [];
     }
     var alreadyExist = false;
     for (var i = 0; i < req.session.dataCardBike.length; i++) {
         if (req.session.dataCardBike[i].name == req.query.bikeNameFromFront) {
-            req.session.dataCardBike[i].quantity = Number(req.session.dataCardBike[i].quantity) + 1;
+            req.session.dataCardBike[i].quantity = req.session.dataCardBike[i].quantity + 1;
             alreadyExist = true;
         }
     }
@@ -40,32 +81,29 @@ router.get('/shop', function(req, res, next) {
             url: req.query.bikeImageFromFront,
             price: req.query.bikePriceFromFront,
             quantity: 1
-        })
+        });
     }
-    res.render('shop', { dataCardBike: req.session.dataCardBike });
+    res.redirect('/shop');
 });
 
 /* 
 ! ******* TRASHBOX ACTIVE ******* */
-router.get('/delete-shop', function(req, res, next) {
+router.get('/delete-shop', async function(req, res, next) {
     if (req.session.dataCardBike == undefined) {
         req.session.dataCardBike = [];
     }
     req.session.dataCardBike.splice(req.query.position, 1)
-    res.render('shop', { dataCardBike: req.session.dataCardBike })
+    res.redirect('/shop');
 })
 
 /* 
  * ******* UPDATE-SHOP ******* */
-router.post('/update-shop', function(req, res, next) {
-    if (req.session.dataCardBike == undefined) {
-        req.session.dataCardBike = [];
-    }
+router.post('/update-shop', async function(req, res, next) {
     var position = req.body.position;
     var newQuantity = req.body.quantity;
-    req.session.dataCardBike[position].quantity = newQuantity;
-    res.render('shop', { dataCardBike: req.session.dataCardBike })
-})
+    req.session.dataCardBike[position].quantity = Number(newQuantity);
+    res.redirect('/shop');
+});
 
 /* 
  * ******* CHECKOUT-SESSION ******* */
@@ -74,11 +112,16 @@ router.post('/create-checkout-session', async(req, res) => {
     if (req.session.dataCardBike == undefined) {
         req.session.dataCardBike = [];
     }
+    var total = calculTotalCmd(req.session.dataCardBike);
+
+    // Frais de port  
+    var montantFraisPort = total.montantFraisPort;
     var stripeItems = [];
-    for (var i = 0; i < req.session.dataCardBike.length; i++) {
+
+    for (var i = 0; i < res.session.dataCardBike.length; i++) {
         stripeItems.push({
-            price_data: {
-                currency: 'eur',
+            price8data: {
+                courrency: 'eur',
                 product_data: {
                     name: req.session.dataCardBike[i].name,
                 },
@@ -87,15 +130,30 @@ router.post('/create-checkout-session', async(req, res) => {
             quantity: req.session.dataCardBike[i].quantity,
         });
     }
+
+    if (montantFraisPort > 0) {
+        stripeItems.push({
+            price_data: {
+                currency: 'eur',
+                product_data: {
+                    name: 'Frais de port',
+                },
+                unit_amount: montantFraisPort * 100,
+            },
+            quantity: 1,
+        });
+    }
+
     const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
+        payement_method_types: ['card'],
         line_items: stripeItems,
-        mode: "payment",
-        success_url: "http://localhost:3000/confirm",
-        cancel_url: "http://localhost:3000/",
+        mode: 'payement',
+        success_url: 'http://localhost:3000/succes',
+        cancel_url: 'http://localhost:3000/'
     });
+
     res.redirect(303, session.url);
-});
+})
 
 
 router.get('/confirm', function(req, res, next) {
